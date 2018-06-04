4072a9c8464a022f5933aea1ef1febdf8f8bc299
Depth <wwwer52@gmail.com>
2018-06-04 20:07:54 +0800

    数据库链接

---
+++ basis/stcase/第一关/demo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+# 生成20个激活码 连接数据库 保存
+
+import random,string,re,MySQLdb
+
+def rand_str(num,length=8): #定义次数与长度
+    for i in range(num):
+        chars =string.ascii_letters+string.digits # 生成小写字母+大写字母+数字
+        s = [random.choice(chars) for i in range(length)] #取随机返回一个字符当中的1个，取长度
+        s = str(s)
+        #s = s.strip('[]')
+        #s = s.replace('\'','')
+        #s = s.replace(',', '')
+        #s = s.replace(' ', '')
+        s = re.sub(r"\W+","",s)
+        print s  #两种方法
+        #数据库部分
+        conn = MySQLdb.connect("localhost","root","你的密码","yaoqingma")
+        cur = conn.cursor() # 方法获取操作
+        sql = 'INSERT INTO list(字段名) VALUES("%s")' % s #需要提前建立好数据库与表
+        cur.execute(sql) # 执行sql语句
+        conn.commit()    # 提交到数据库
+
+if __name__ =='__main__':
+    rand_str(5)  #自己生效
